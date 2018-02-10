1c7a7239b353e406f303c09339c4b029d502b9d3
Tomasz Czmyr <tomasz.czmyr@bilansjr.pl>
2018-02-10 09:12:07 +0100

    Domain

---
+++ src/main/java/guru/springframework/api/User.java
@@ -0,0 +1,16 @@
+package guru.springframework.api;
+
+public class User {
+
+    private String gender;
+    private Name name;
+    private Location location;
+    private String email;
+    private Login login;
+    private String phone;
+    private Job job;
+    private Billing billing;
+    private String language;
+    private String currency;
+
+}
---
+++ src/main/java/guru/springframework/api/domain.java
@@ -0,0 +1,4 @@
+package guru.springframework.api;
+
+public class domain {
+}
